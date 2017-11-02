a7cf8b7245e1398993954de4fc5788d8d4d435fa
Jessica Bensch <github email address>
2017-11-02 15:20:52 -0400

    Improve output code

--- ticket.rb
+++ ticket.rb
@@ -24,13 +24,18 @@ def ticket.price
   5.50
 end
 
-print "This ticket is for: "
-print ticket.event + ", at "
-print ticket.venue + ", on "
-puts ticket.date + "."
-print "The performer is "
-puts ticket.performer + "."
-print "The seat is "
-print ticket.seat + ", "
-print "and it costs $"
-puts "%.2f." % ticket.price
+#print "This ticket is for: "
+#print ticket.event + ", at "
+#print ticket.venue + ", on "
+#puts ticket.date + "."
+#print "The performer is "
+#puts ticket.performer + "."
+#print "The seat is "
+#print ticket.seat + ", "
+#print "and it costs $"
+#puts "%.2f." % ticket.price
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}." +
+  "The performer is #{ticket.performer}." +
+  "The seat is #{ticket.seat}, " +
+  "and it costs $#{"%.2f." % ticket.price}"
